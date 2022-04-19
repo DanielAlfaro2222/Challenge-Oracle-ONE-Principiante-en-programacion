74f3b2da7ff0f9975d291414490b30a5eeb6afb6
Daniel Alfaro <kdalfaro@misena.edu.co>
2022-04-19 17:15:42 -0500

    Chore: Se sube el proyecto terminado al 100%

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const containerArticleInfo = document.getElementById('article-text-info');
 const containerArticleResult = document.getElementById('article-text-result');
 const mensajeEncriptado = document.getElementById('result-encrypt-text');
 const botonCopiarTextoEncriptado = document.getElementById('btn-copiar-texto-encriptado');
+const botonLimpiar = document.getElementById('btn-limpiar-todo');
 
 function encriptarTexto(texto) {
     let caracteres = texto.split('');
@@ -33,24 +34,58 @@ function desencriptarTexto(texto) {
     };
 
     for (let key in letrasEncriptadas) {
-        texto = texto.replace(key, letrasEncriptadas[key]);
+        texto = texto.replaceAll(key, letrasEncriptadas[key]);
     }
-  
+
   return texto;
 }
 
-botonEncriptarTexto.addEventListener('click', () => {
-    containerArticleInfo.classList.add('container-info-result-encrypt-text--ocult');
-    containerArticleResult.classList.add('container-info-result-encrypt-text--show');
+function validarDatosDeEntrada(texto) {
+    const caracteresNoPermitidos = "@©°ñ«»—¿½®™§¶†‡±~µ÷·•Ø‰_:Ñ][¨*¡=)(/&%$#æ@ł€¶ŧ←↓→øþłĸłĸ̉ħŋħŋŋđđððßææ«»¢“”nµ─·̣áéíóú";
 
-    mensajeEncriptado.value = encriptarTexto(mensajeAEncriptar.value.toLowerCase().trim());
+    let textoCorrecto = true;
+
+    for (let palabra of texto.split('')) {
+        if (caracteresNoPermitidos.includes(palabra)) {
+            textoCorrecto = false;
+            return textoCorrecto;
+        }
+    }
+
+    return textoCorrecto;
+}
+
+botonEncriptarTexto.addEventListener('click', () => {
+    if (validarDatosDeEntrada(mensajeAEncriptar.value)) {
+        containerArticleInfo.classList.add('container-info-result-encrypt-text--ocult');
+        containerArticleResult.classList.add('container-info-result-encrypt-text--show');
+    
+        mensajeEncriptado.value = encriptarTexto(mensajeAEncriptar.value.toLowerCase().trim());
+    } else {
+        alert('No se permiten caracteres especiales o letras con acento, por favor corrija el texto');
+    }
 });
 
 botonCopiarTextoEncriptado.addEventListener('click', () => {
     mensajeEncriptado.select();
     document.execCommand('copy');
+    alert('Texto copiado');
 });
 
 botonDesencriptarTexto.addEventListener('click', () => {
-    mensajeEncriptado.value = desencriptarTexto(mensajeAEncriptar.value.toLowerCase().trim());
+    if (validarDatosDeEntrada(mensajeAEncriptar.value)) {
+        mensajeEncriptado.value = desencriptarTexto(mensajeAEncriptar.value.toLowerCase().trim());
+        containerArticleInfo.classList.add('container-info-result-encrypt-text--ocult');
+        containerArticleResult.classList.add('container-info-result-encrypt-text--show');
+    } else {
+        alert('No se permiten caracteres especiales o letras con acento, por favor corrija el texto');
+    }
+
+});
+
+botonLimpiar.addEventListener('click', () => {
+    mensajeAEncriptar.value = '';
+    containerArticleInfo.classList.remove('container-info-result-encrypt-text--ocult');
+    containerArticleResult.classList.remove('container-info-result-encrypt-text--show');
+    setTimeout("alert('Pantalla limpiada con exito')", 200);
 });
